bbff106879d6e04885cc5c40555ae087d1178535
gxrys <garysohjy@gmail.com>
2022-04-08 15:08:10 +0800

    react mock response for results page

--- FrontEnd/src/components/Results.js
+++ FrontEnd/src/components/Results.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Box, Typography, Card, Button } from "@mui/material";
 import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
 import FlightLandIcon from "@mui/icons-material/FlightLand";
+import MockKL from "../mockResponse/KL.json";
+import MockXRY from "../mockResponse/XRY.json";
+
 const Results = () => {
   return (
     // Origin -> destination flight details
@@ -11,7 +14,10 @@ const Results = () => {
         display="flex"
         justifyContent="left"
       >
-        <Typography variant="h5">1. Singapore to Jerez</Typography>
+        <Typography variant="h5">
+          1. {MockKL.flights[0].originAirportCode} to{" "}
+          {MockKL.flights[0].destinationAirportCode}
+        </Typography>
       </Box>
 
       <Card
